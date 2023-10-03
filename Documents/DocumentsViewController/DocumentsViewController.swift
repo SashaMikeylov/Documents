c2ce9692a725f4438a66b9b4b35609ceb2079fd4
Sanek <sashaloplop@mail.ru>
2023-10-03 23:04:08 +0300

    Initial commit

---
+++ Documents/DocumentsViewController/DocumentsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DocumentsViewController.swift
+//  Documents
+//
+//  Created by Денис Кузьминов on 03.10.2023.
+//
+
+import UIKit
+
+final class DocumentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
